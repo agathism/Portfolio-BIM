59682bca6f26698578bdf1104881891ce5ad0c91
agathism <soumare.aicha13@gmail.com>
2025-11-20 00:57:34 +0100

    first push

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-nav',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavBarComponent {
+
+}
